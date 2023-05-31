d0d36d5b6ebf513bc1004c74b450d5e93fb22d5e
daniel <danieltabrizian@live.nl>
2023-05-31 13:29:54 +0200

    Cleaned front-end a bit

--- vue-render/src/App.vue
+++ vue-render/src/App.vue
@@ -1,85 +1,80 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
+
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <canvas  style="width: 800px;height: 500px" ref="chart"></canvas>
+  <HelloWorld msg="sfddsf"/>
 </template>
 
+<script>
+
+import HelloWorld from "@/components/HelloWorld.vue";
+import Chart from 'chart.js/auto';
+
+export default {
+  components: {HelloWorld},
+  data(){
+    return{
+      data: [
+    { year: 2010, count: 10 },
+    { year: 2011, count: 20 },
+    { year: 2012, count: 15 },
+    { year: 2013, count: 25 },
+    { year: 2014, count: 22 },
+    { year: 2015, count: 30 },
+    { year: 2016, count: 28 },
+  ]
+    }
+  },
+  mounted() {
+    this.setup();
+  },
+  methods:{
+    setup(){
+      new Chart(
+        this.$refs.chart,
+        {
+          type: 'line',
+          options:{
+            scales: {
+              x: {
+                title: {
+                  display: true,
+                  text: 'Month'
+                }
+              },
+              y: {
+                stacked: true,
+                title: {
+                  display: true,
+                  text: 'Value'
+                }
+              }
+            },
+          },
+          data: {
+            labels: this.data.map(row => row.year),
+            datasets: [
+              {
+                label: 'Acquisitions by year',
+                data: this.data.map(row => row.count),
+                fill: true,
+                stacked: true
+              },
+                {
+                label: 'Acquisitions by year',
+                data: this.data.map(row => row.count),
+                fill: true,
+                  stacked: true
+              }
+            ]
+          }
+        }
+      );
+    }
+  }
+}
+</script>
+
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
 </style>
