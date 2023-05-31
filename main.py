0cec64cca71e65af4d95653a934515e2079b9972
672593 <anel.gusinac@gmail.com>
2023-05-31 03:15:00 +0200

    Added model validation endpoint, creates json with validation information. Added setup file with all dependencies.

--- main.py
+++ main.py
@@ -1,7 +1,32 @@
 from flask import Flask, render_template, jsonify, send_from_directory
 import os
+import json
+import cv2
+import collections, numpy
+from ultralytics import YOLO
+from glob import glob
+from PIL import Image
 
 app = Flask(__name__)
+
+parsed_data = {}
+model = YOLO("detect/detect/train9/weights/SCDModelV1.pt")  # build a new model from scratch
+# model.export(format='onnx')
+image_folder = r"pictures"  # Path to the folder containing images
+output_folder = r"output"  # Path to the folder where you want to save the predicted images
+
+
+def create_json_object(results, class_labels, title):
+    new_object = {"title": title}
+
+    for result in results:
+        counter = collections.Counter(result.boxes.cls.numpy())
+        for key in counter:
+            new_object[class_labels[key]] = counter[key]
+            print(class_labels[key], '->', counter[key])
+
+    parsed_data[len(parsed_data) + 1] = new_object
+
 
 @app.route('/')
 def index():
@@ -15,8 +40,30 @@ def public_files(filename):
 
 @app.route('/endpoint1')
 def endpoint1():
-    data = {'message': 'Hello from endpoint1'}
-    return jsonify(data)
+    # Create the output folder if it doesn't exist
+    os.makedirs(output_folder, exist_ok=True)
+
+    # Get a list of image files in the folder
+    image_files = glob(os.path.join(image_folder, "*.jpg"))  # Modify the file extension if necessary
+
+    # Go through the acquired images and add their details to JSON object by calling show_amount function
+    for image_file in image_files:
+        # Load the image
+        image = Image.open(image_file)
+        image_title = os.path.splitext(os.path.basename(image_file))[0]
+
+        # Make predictions on the image
+        results = model.predict(image, save=True, show=False)  # Predict on the image
+        create_json_object(results, model.names, image_title)
+
+    json_data = json.dumps(parsed_data)
+    file_path = "output\output.json"
+
+    with open(file_path, 'w') as file:
+        file.write(json_data)
+
+    cv2.waitKey(0)
+    return jsonify(json_data)
 
 @app.route('/endpoint2')
 def endpoint2():
