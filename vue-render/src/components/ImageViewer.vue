ad97852b95861fa2920eedd87e554cacc4a035a4
daniel <danieltabrizian@live.nl>
2023-06-02 06:41:13 +0200

    Made Chart up to spec, only need to register click event and load image and prepare backend endpoints to serve images

---
+++ vue-render/src/components/ImageViewer.vue
@@ -0,0 +1,18 @@
+<script setup>
+defineProps({
+  image: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div class="greetings">
+    <h1 class="green">{{ image }}</h1>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
